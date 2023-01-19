d320eec789ac63797bdc2d13485dad5e77e1d9dd
Tanakoneiei <76490244+Tanakoneiei@users.noreply.github.com>
2023-01-19 23:30:47 +0700

    pokedex1

---
+++ lib/pokemon_detail_screen.dart
@@ -0,0 +1,55 @@
+// ignore_for_file: unused_import
+
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:project_pokemon/pokemon_model.dart';
+import 'package:project_pokemon/pokemon_service.dart';
+import 'pokemon_list_model.dart';
+import 'package:getwidget/getwidget.dart';
+
+class PokemonDetailScreen extends StatelessWidget {
+  PokemonDetailScreen({required this.title, required this.url});
+  String title;
+  String url;
+  int number = 11;
+  PokemonService pokemonService = PokemonService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(this.title),
+        ),
+        body: FutureBuilder(
+          future: pokemonService.getdata(url),
+          builder: (context, AsyncSnapshot<PokemonModel> snapshot) {
+            if (snapshot.hasData) {
+              PokemonModel data = snapshot.data!;
+              return Scaffold(
+                body: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GFAvatar(
+                        backgroundColor: Color.fromARGB(2255, 186, 131, 97),
+                        backgroundImage: NetworkImage(
+                          data.sprites!.frontDefault.toString(),
+                        ),
+                        shape: GFAvatarShape.standard,
+                        size: 150,
+                      ),
+                      Text("Name: " + data.name!),
+                      Text("ID: " + data.id.toString()),
+                      Text("Weight: " + data.weight.toString()),
+                    ],
+                  ),
+                ),
+              );
+            } else {
+              return CircularProgressIndicator();
+            }
+          },
+        ));
+  }
+}
